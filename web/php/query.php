19296c413ea9bab3b68f78ee7c546309eb6c6dce
leobiriba <leonardoxcontato@gmail.com>
2021-08-30 23:45:53 -0300

    Hoje terminei de fazer o insert e select ao banco de dados, utilizando codigo para combater o sql inject

--- web/php/query.php
+++ web/php/query.php
@@ -1,9 +1,51 @@
 <?php
-include('connectionDb.php');
-$db= new ControllerDb();
+  class ControllerDb{
+
+    private $host = "ec2-23-21-229-200.compute-1.amazonaws.com";
+    private $user = "denibmcnmjikfy";
+    private $password  = "18201ee8394165431162a91c26243c1dcf20092899f9d94ad1d8587604e88148";
+    private $database = "d4vk0o4kdhima3";
+    private $conn;
+
+    function connectDb(){
+        $conn = pg_connect("host=$this->host dbname=$this->database user=$this->user password=$this->password")or 
+        die("Failed to create connection to database: ". pg_last_error(). "<br/>");
+    }
+    function construct(){
+        $this->conn = $this->connectDb();
+    }
+    function accessQuery($query){
+        $result = pg_query($query);
+        while($row=pg_fetch_assoc($result)){
+            
+        }
+        if(!empty($resultset))
+        return $resultset;
+    }
+}
+
+$db = new ControllerDb();
 $db->connectDb();
 
-if(isset($_POST['btnCadastrarChaveiro'])){
+if(isset($_POST['btnCadastrarChaveiro'])){  
+    
+    if(!empty($_POST['txtDescricao'])){
+        $descricao = $_POST['txtDescricao'];
+    } else {
+        $descricao = "Chaveiro pronto a serviço!";
+    }
+
+    if(!empty($_POST['txtPagamentoD']) && !empty($_POST['txtPagamentoC'])){
+        $pagamento = $_POST['txtPagamentoD'] ." e ". $_POST['txtPagamentoC'];
+    } elseif(!empty($_POST['txtPagamentoD'])){
+        $pagamento = $_POST['txtPagamentoD'];
+    } elseif(!empty($_POST['txtPagamentoC'])){
+        $pagamento = $_POST['txtPagamentoC'];
+    } else{
+
+    }
+
+    //Verificando se os campos estão vazios.
 
     $name = $_POST['txtName'];
     $email = $_POST['txtEmailCadastro'];
@@ -12,9 +54,9 @@ if(isset($_POST['btnCadastrarChaveiro'])){
     $dataN = $_POST['txtDataNascimento'];
     $cep =  $_POST['txtCep'];
     $tel = $_POST['txtTelefone'];
-    $descricao = $_POST['txtDescricao'];
+    $descricao;
     $especialidade = $_POST['txtEspecialidade'];
-    $pagamento = $_POST['txtPagamento'];
+    $pagamento;
 
     try{
     $db->accessQuery(
@@ -29,17 +71,18 @@ if(isset($_POST['btnCadastrarChaveiro'])){
 
 } elseif(isset($_POST['btnCadastrarCliente'])){
         
-    $name = $_POST['txtName'];
-    $email = $_POST['txtEmailCadastro'];
-    $senha = $_POST['txtSenhaCadastro'];
-    $cpf = $_POST['txtCpf'];
-    $dataN = $_POST['txtDataNascimento'];
-    $tel = $_POST['txtTelefone'];
+    $name = addslashes($_POST['txtName']);
+    $email = addslashes($_POST['txtEmailCadastro']);
+    $senha = addslashes($_POST['txtSenhaCadastro']);
+    $cpf = addslashes($_POST['txtCpf']);
+    $dataN = addslashes($_POST['txtDataNascimento']);
+    $tel = addslashes($_POST['txtTelefone']);
+
     try{
         $db->accessQuery(
         $query="insert into Cliente(nome, email, telefone, cpf, senha, datadenascimento) 
-        values('$name', '$email', '$tel', '$cpf', '$senha', '$dataN');");
-         header('location: ../index.php');
+        values('$name', '$email','$tel','$cpf','$senha','$dataN');");
+        header('location: ../index.php');
         return $query;
     } catch(Exception $e){
        return $e;
@@ -48,20 +91,20 @@ if(isset($_POST['btnCadastrarChaveiro'])){
 
 } elseif(isset($_POST['btnLogin'])){
 
-    $email = $_POST["txtEmailLogin"];
-    $senha = $_POST["txtSenhaLogin"];
+    $email = addslashes($_POST["txtEmailLogin"]);
+    $senha = addslashes($_POST["txtSenhaLogin"]);
     // error_reporting(0);
     // ini_set("display_erros", 0);
     try{
-    $db->accessQuery($query="select email from Chaveiro;");
-
+    $db->accessQuery($query=sprintf("select email, senha from Chaveiro where email= '%s' and senha=%d;", $email, $senha));
     session_start();
     $_SESSION['login']  = $db->accessQuery($query);
-
-    }catch(Exception $e){
+    setcookie("primerioUsuario", "Bem vindo");
+    header("location:../index.php");
+        }catch(Exception $e){
         return $e;
     };
-    header('location: ../index.php');
+    
 }; 
 
 // Esse código foi feito para quando aperter certo button, ele irá corresponder 
