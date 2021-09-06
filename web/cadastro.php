dd91d159c4c3ad6e9990d00e12992012fd9dc09b
leobiriba <leonardoxcontato@gmail.com>
2021-09-06 17:45:38 -0300

    Fazendo otimizaçoes ao codigo

--- web/cadastro.php
+++ web/cadastro.php
@@ -12,6 +12,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+        <script src="//code-sa1.jivosite.com/widget/PNSgAqbd3B" async></script>
  
         <!-- <script src="chamarFunctionPhp.js"></script> -->
         <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
