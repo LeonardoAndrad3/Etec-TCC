2a33eaa1856c3ef50fc826c09cd891afcf54649d
mateussantanasilva <santanasilva1778@gmail.com>
2021-09-16 16:22:02 -0300

    Foi criado a tela de contate além das demais, foi finalizado os ajustes nas demais telas também

--- web/js/script.js
+++ web/js/script.js
@@ -1,11 +1,30 @@
 // Scroll suave para link interno
-$('.home a').click(function(e){
+//triplicado pois cada um tem uma posição diferente, necessitando reduzir px individualmente
+$('.home .botoes-verdes').click(function(e){ //saber mais
 	e.preventDefault();
 	var id = $(this).attr('href'),
 			menuHeight = $('nav').innerHeight(),
 			targetOffset = $(id).offset().top;
 	$('html, section').animate({
 		scrollTop: targetOffset
+	}, 500);
+});
+$('.home .botoes').click(function(e){ //porque contratar
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, section').animate({
+		scrollTop: targetOffset -80
+	}, 500);
+});
+$('.menu .link-parascroll').click(function(e){ //sobre-nos
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, section').animate({
+		scrollTop: targetOffset -65
 	}, 500);
 });
 
