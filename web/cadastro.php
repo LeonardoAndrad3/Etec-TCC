03435513ae24a20d5fc3b2cebd3c97215286f849
leobiriba <leonardoxcontato@gmail.com>
2021-09-19 16:49:52 -0300

    Fazendo teste

--- web/cadastro.php
+++ web/cadastro.php
@@ -83,11 +83,6 @@
                         <li><p id="funcao">*Função Principal <br/>
                             <select id="lista" name="txtEspecialidade"required>
                                 <option value="" disabled selected>Selecione</option>
-                                <?php   
-                                include("./php/query.php");
-                                $db = new ControllerDb();
-                                $db->profissao()                              
-                                ?>
                                 <option value="3">Outro</option>
                             </select><br/>
                         </p></li>
